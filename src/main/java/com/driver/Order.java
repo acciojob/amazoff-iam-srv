4b3f4609b89ec2b14fecf023d584f88d191515b5
sriram viswanathan <sriramviswanathan2@gmail.com>
2023-02-12 00:41:47 +0530

    Solution1

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+import io.swagger.models.auth.In;
+
 public class Order {
 
     private String id;
@@ -9,7 +11,20 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id = id;
+        Integer hour = Integer.valueOf(deliveryTime.substring(0, 2));
+        Integer minutes = Integer.valueOf(deliveryTime.substring(3));
+        this.deliveryTime = hour*60 + minutes;
     }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public void setDeliveryTime(String deliveryTime) {
+//        int DeliveryTime = ((Integer.parseInt(deliveryTime.substring(0, 2)) )* 60) + Integer.parseInt(deliveryTime.substring(3, 5));
+//        this.deliveryTime = DeliveryTime;
+//    }
 
     public String getId() {
         return id;
